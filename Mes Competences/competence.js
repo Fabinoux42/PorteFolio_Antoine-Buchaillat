35e63a46e5820e323b3bb1185a5770b8e4731718
Fabinoux42 <fabien.varoteauxlavigne@gmail.com>
2022-10-30 22:09:36 +0100

    Enlevage du bouton replay (ta mere)

--- Mes Competences/competence.js
+++ Mes Competences/competence.js
@@ -1,13 +1,5 @@
-/*bouton replay*/
-const ouvertureSite = document.querySelector(".ouvertureSite");
-
 function refresh() {
   ouvertureSite.style.display="block";
-}
-
-function handleReplay() {
-  ouvertureSite.style.display="none";
-  setTimeout(refresh, 50);
 }
 
 setTimeout(()=>ouvertureSite.style.display="none", 5000)
